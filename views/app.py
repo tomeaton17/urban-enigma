ba83a76676d752cd3662d043effd04f2f9c2eef3
bryoh <brianbryo@gmail.com>
2017-10-12 00:38:06 +0100

    We can print something on the page now! YAY

--- views/app.py
+++ views/app.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template
-
-
+import instagram as my_g
+import os
 app = Flask(__name__, template_folder='../templates')
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    user_name = os.environ['USER_NAME']
+    bryoh = my_g.Profile(user_name)
+
+    return render_template('index.html',
+                          div=bryoh)
+
 
 if(__name__ == '__main__'):
     app.run(debug=True)
